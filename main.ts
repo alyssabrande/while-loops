7dda146cec8ab93ad28f9388df4562d5a73bc18b
alyssabrande <102975794+alyssabrande@users.noreply.github.com>
2022-04-08 08:14:42 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    while (count < 10) {
+        basic.pause(1000)
+        basic.showNumber(count)
+        count += 1
+    }
+})
+let count = 0
+count = 0
+basic.forever(function () {
+	
+})
